996140406a8f0eff0120b6566d5cbf3425a3cd28
Tomislav Koretic <opel.tigra16@gmail.com>
2019-07-07 17:50:56 +0200

    JS first task

--- worktask/src/components/fetchBeer/FetchBeer.js
+++ worktask/src/components/fetchBeer/FetchBeer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./FetchBeerStyle.css";
 
 export default class FetchBeer extends React.Component {
 
@@ -22,9 +23,16 @@ export default class FetchBeer extends React.Component {
           <div>
            {this.state.beers.map((beer) => {
              return <div key={beer.id}>
-               <h1>{beer.name}</h1>
-               <p>{beer.tagline}</p>
+               <h1 className="h1" >NAME : {beer.name}</h1>
                <img src= {beer.image_url}/>
+               <h2>TAGLINE : {beer.tagline}</h2>
+               <p>FIRST BREWED : {beer.first_brewed}</p>
+               <p> DESCRIPTION : <br></br>{beer.description}</p>
+               <p> FOOD PAIRING : <br></br>{beer.food_pairing}</p>
+               <p> ALCOHOL BY VOLUME(%) : {beer.abv}</p>
+               <p> pH : {beer.ph}</p>
+               
+               
                </div>
             
            })}
